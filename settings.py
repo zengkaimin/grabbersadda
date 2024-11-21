3c711a8febde216962e49910ce341584a6969ef9
zengkaimin <zengkaimin1991@gmail.com>
2024-11-21 17:18:47 +0800

    20241121

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# Default auto field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
